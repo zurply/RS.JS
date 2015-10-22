241bf38f285265d7be21400d6e633181423c7340
zurply <ovidiu.tudorache@zurply.com>
2015-10-22 14:54:45 +0300

    1.0.0

--- RS.JS.js
+++ RS.JS.js
@@ -679,3 +679,20 @@ Handlebars.registerHelper('json', function (context) {
 Handlebars.registerHelper('log', function (variable) {
     console.log(variable);
 });
+
+RS.HandlebarsRenderer = (function () {
+
+    return {
+        Render: function (data, template, container, callback, append) {
+            data = data || [];
+            var html = template({ data: data });
+
+            if (append)
+                container.append(html);
+            else container.html(html);
+
+            if (callback)
+                callback();
+        }
+    };
+})();
